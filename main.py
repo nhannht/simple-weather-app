0d488bb202faef3c67b68334d06c6478aeca8d1c
nhannht <nhanclassroom@gmail.com>
2021-06-12 23:08:05 +0700

    minor change

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from flask import Flask
 from flask import redirect
 from flask import render_template
 from flask import request
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask import flash
-from model import key
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+key = '35a80801f782daf9eb0622b17269a46c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 db = SQLAlchemy(app)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String, unique=True, nullable=False)
@@ -35,24 +35,23 @@ def fetch_from_openweather_and_insert_to_db(city):
     status = req.ok
     if status:
         respond = req.json()
-        degree = respond['main']['temp'] -275.13
-        degree = round(degree,2)
+        degree = respond['main']['temp'] - 275.13
+        degree = round(degree, 2)
         state = respond['weather'][0]['description']
         city = City(city=city, degree=degree, state=state)
         db.session.add(city)
         try:
-            flash("The city doesn't exist!")
             db.session.commit()
         except IntegrityError:
             flash("The city has already been added to the list!")
             db.session.rollback()
     else:
-        flash("The city doesn't exist!")
+        flash("The city doesn't exist in the world!")
+
 
 def fetch_all_data():
     out = City.query.all()
     return out
-
 
 @app.route('/', methods=['GET', 'POST'])
 def profile():
@@ -72,7 +71,7 @@ def profile():
 def delete(id_city):
     city = City.query.filter_by(id=id_city).first()
     if city is None:
-        flash("The city doesn't exist")
+        flash("The city doesn't exist in database")
         return redirect('/')
     db.session.delete(city)
     db.session.commit()
@@ -83,11 +82,9 @@ def delete(id_city):
 def internal_server_erro(error):
     return redirect('/')
 
-
-@app.route('/login')
-def log_in():
-    return 'This is login page'
-
+@app.errorhandler(404)
+def internal_server_erro(error):
+    return redirect('/')
 
 # don't change the following way to run flask:
 if __name__ == '__main__':
--- model.py
+++
@@ -1,4 +0,0 @@
-import requests
-from collections import deque
-key = '35a80801f782daf9eb0622b17269a46c'
-cities = deque(maxlen=3)
